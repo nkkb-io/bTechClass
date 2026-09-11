10fc6720982035c9c9b0243b6597d50ce662862b
nkkb-io <e.kyei2406@gmail.com>
2026-09-11 18:55:08 +0000

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,12 @@
+// profile.js
+// Simple script that logs basic profile info to the console.
+
+const profile = {
+  name: "Nana",
+  age: 25, // change this to your actual age
+  favoriteLanguage: "JavaScript" // change this to your favorite language
+};
+
+console.log(`Name: ${profile.name}`);
+console.log(`Age: ${profile.age}`);
+console.log(`Favorite Programming Language: ${profile.favoriteLanguage}`);
